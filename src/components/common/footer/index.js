b43518805ae6db2fbcfdbba546782dd0ffcfcccf
muhammadzubairmoosani <muhammadzubairmoosani@gmail.com>
2024-03-22 02:58:46 +0500

    add temporary dark-mode button in footer

--- src/components/common/footer/index.js
+++ src/components/common/footer/index.js
@@ -7,6 +7,7 @@ import {
   Heading,
   TextField,
   Logo,
+  ThemeSwitch,
 } from "../index";
 import { useTheme } from "next-themes";
 
@@ -115,6 +116,7 @@ export const Footer = () => {
           <Paragraph className="text-gray text-lg font-medium">
             A Product of Nexovia Digital
           </Paragraph>
+          <ThemeSwitch />
         </div>
       </Container>
     </div>
